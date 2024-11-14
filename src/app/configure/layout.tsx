6b98640f7358e58f48d1a12ac1053a3540dc4dfb
Aman Jain <amansjain.aj@gmail.com>
2024-11-14 14:04:46 +0530

    add case ordering stepper

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,13 @@
 import { ReactNode } from 'react';
 import MaxWithWrapper from '@/components/custom/MaxWithWrapper';
+import Steps from '@/components/custom/Steps';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <MaxWithWrapper className="flex flex-col flex-1">{children}</MaxWithWrapper>
+    <MaxWithWrapper className="flex flex-col flex-1">
+      <Steps />
+      {children}
+    </MaxWithWrapper>
   );
 };
 
