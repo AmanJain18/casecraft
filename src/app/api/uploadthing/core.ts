cd51f0a8134e0dc5b4b9170efde3ea938cf78815
Aman Jain <amansjain.aj@gmail.com>
2024-11-15 22:32:48 +0530

    update and enhance image upload logic

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,22 +1,51 @@
+import { updateConfiguration } from './../../../db/index';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 import * as z from 'zod';
+import sharp from 'sharp';
+import { createConfiguration } from '@/db';
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: '8MB' } })
-    .input(
-      z.object({
-        configId: z.string().optional(),
-      })
-    )
-    .middleware(async ({ input }) => {
-      return { input };
-    })
-    .onUploadComplete(async ({ metadata }) => {
-      const { configId } = metadata.input;
-      return { configId };
-    }),
+    imageUploader: f({ image: { maxFileSize: '8MB' } })
+        .input(
+            z.object({
+                configId: z.string().optional(),
+            }),
+        )
+        .middleware(async ({ input }) => {
+            return { input };
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            const { configId } = metadata.input;
+
+            const res = await fetch(file.url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const buffer = await res.arrayBuffer();
+            const imageMetadata = await sharp(buffer).metadata();
+
+            const { width, height } = imageMetadata;
+
+            if (!configId) {
+                const configuration = await createConfiguration(
+                    width || 500,
+                    height || 500,
+                    file.url,
+                );
+                return { configId: configuration.id };
+            } else {
+                const updatedConfiguration = await updateConfiguration(
+                    configId,
+                    file.url,
+                );
+
+                return { configId: updatedConfiguration?.id };
+            }
+        }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -10,132 +10,134 @@ import { useRouter } from 'next/navigation';
 import { useToast } from '@/hooks/use-toast';
 
 const Page = () => {
-  const { toast } = useToast();
-  const [isDragOver, setIsDragOver] = useState<boolean>(false);
-  const [uploadProgress, setUploadProgress] = useState<number>(0);
-  // const [files, setFiles] = useState<File[]>([]);
-  const [isPending, startTransition] = useTransition();
+    const { toast } = useToast();
+    const [isDragOver, setIsDragOver] = useState<boolean>(false);
+    const [uploadProgress, setUploadProgress] = useState<number>(0);
+    const [isPending, startTransition] = useTransition();
 
-  const router = useRouter();
+    const router = useRouter();
 
-  const { startUpload, isUploading } = useUploadThing('imageUploader', {
-    onClientUploadComplete: ([data]) => {
-      const { configId } = data.serverData;
-      startTransition(() => {
-        router.push(`/configure/design?id=${configId}`);
-      });
-    },
-    onUploadProgress: (progress) => {
-      setUploadProgress(progress);
-    },
-    onUploadError: (error) => {
-      console.log(error);
-    },
-  });
-  const onDropRejected = (rejectedFiles: FileRejection[]) => {
-    const [firstFile] = rejectedFiles;
-    setIsDragOver(false);
-    if (firstFile.errors[0].code === 'file-too-large') {
-      toast({
-        title: 'File is too large',
-        description: 'Please choose a file under 8MB',
-        variant: 'destructive',
-      });
-    } else if (firstFile.errors[0].code === 'file-invalid-type') {
-      toast({
-        title: `${firstFile.file.type} type is not supported`,
-        description:
-          'Please choose a image from the supported formats: PNG, JPG, JPEG',
-        variant: 'destructive',
-      });
-    }
-  };
-  const onDropAccepted = (acceptedFiles: File[]) => {
-    startUpload(acceptedFiles, {
-      configId: undefined,
+    const { startUpload, isUploading } = useUploadThing('imageUploader', {
+        onClientUploadComplete: ([data]) => {
+            const { configId } = data.serverData;
+            startTransition(() => {
+                router.push(`/configure/design?id=${configId}`);
+            });
+        },
+        onUploadProgress: (progress) => {
+            setUploadProgress(progress);
+        },
     });
-    setIsDragOver(false);
-  };
+    const onDropRejected = (rejectedFiles: FileRejection[]) => {
+        const [firstFile] = rejectedFiles;
+        setIsDragOver(false);
+        if (firstFile.errors[0].code === 'file-too-large') {
+            toast({
+                title: 'File is too large',
+                description: 'Please choose a file under 8MB',
+                variant: 'destructive',
+            });
+        } else if (firstFile.errors[0].code === 'file-invalid-type') {
+            toast({
+                title: `${firstFile.file.type} type is not supported`,
+                description:
+                    'Please choose a image from the supported formats: PNG, JPG, JPEG',
+                variant: 'destructive',
+            });
+        }
+    };
+    const onDropAccepted = (acceptedFiles: File[]) => {
+        startUpload(acceptedFiles, {
+            configId: undefined,
+        });
+        setIsDragOver(false);
+    };
 
-  return (
-    <div
-      className={cn(
-        'relative h-full w-full flex-1 my-16 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center',
-        { 'ring-blue-900/10 bg-blue-900/10': isDragOver }
-      )}
-    >
-      <div className="relative flex flex-1 flex-col justify-center items-center w-full">
-        <Dropzone
-          onDragEnter={() => setIsDragOver(true)}
-          onDragLeave={() => setIsDragOver(false)}
-          onDropAccepted={onDropAccepted}
-          onDropRejected={onDropRejected}
-          accept={{
-            'image/*': ['.png', '.jpg', '.jpeg'],
-          }}
+    return (
+        <div
+            className={cn(
+                'relative my-16 flex h-full w-full flex-1 flex-col items-center justify-center rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl',
+                { 'bg-blue-900/10 ring-blue-900/10': isDragOver },
+            )}
         >
-          {({ getRootProps, getInputProps }) => (
-            <div
-              {...getRootProps()}
-              className="flex flex-1 flex-col items-center justify-center w-full h-full"
-            >
-              <input {...getInputProps()} />
-              <div className="text-center">
-                <div className="flex flex-col items-center justify-center">
-                  {isDragOver ? (
-                    <>
-                      <MousePointerSquareDashed className="w-10 h-10 text-primary" />
-                      <p className="mt-2 text-sm text-zinc-700">
-                        <span className="font-semibold">Drop file</span> to
-                        upload
-                      </p>
-                    </>
-                  ) : isUploading || isPending ? (
-                    <>
-                      <Loader2 className="w-10 h-10 text-primary animate-spin" />
-                      {isUploading ? (
-                        <>
-                          <p className="mt-2 text-sm text-zinc-700">
-                            Uploading...
-                          </p>
-                          <Progress
-                            value={uploadProgress}
-                            className="mt-2 h-2 w-40 bg-gray-300"
-                          />
-                        </>
-                      ) : (
-                        <p className="mt-2 text-sm text-zinc-700">
-                          Redirecting, please wait...
-                        </p>
-                      )}
-                    </>
-                  ) : (
-                    <>
-                      <UploadIcon className="w-10 h-10 text-primary" />
-                      <p className="mt-2 text-sm text-zinc-700">
-                        <span className="font-semibold">Drag and drop</span>{' '}
-                        your image here, or{' '}
-                        <span className="font-semibold">
-                          click here to browse
-                        </span>
-                      </p>
-                    </>
-                  )}
+            <div className='relative flex w-full flex-1 flex-col items-center justify-center'>
+                <Dropzone
+                    onDragEnter={() => setIsDragOver(true)}
+                    onDragLeave={() => setIsDragOver(false)}
+                    onDropAccepted={onDropAccepted}
+                    onDropRejected={onDropRejected}
+                    accept={{
+                        'image/*': ['.png', '.jpg', '.jpeg'],
+                    }}
+                >
+                    {({ getRootProps, getInputProps }) => (
+                        <div
+                            {...getRootProps()}
+                            className='flex h-full w-full flex-1 flex-col items-center justify-center'
+                        >
+                            <input {...getInputProps()} />
+                            <div className='text-center'>
+                                <div className='flex flex-col items-center justify-center'>
+                                    {isDragOver ? (
+                                        <>
+                                            <MousePointerSquareDashed className='h-10 w-10 text-primary' />
+                                            <p className='mt-2 text-sm text-zinc-700'>
+                                                <span className='font-semibold'>
+                                                    Drop file
+                                                </span>{' '}
+                                                to upload
+                                            </p>
+                                        </>
+                                    ) : isUploading || isPending ? (
+                                        <>
+                                            <Loader2 className='h-10 w-10 animate-spin text-primary' />
+                                            {isUploading ? (
+                                                <>
+                                                    <p className='mt-2 text-sm text-zinc-700'>
+                                                        Uploading...
+                                                    </p>
+                                                    <Progress
+                                                        value={uploadProgress}
+                                                        className='mt-2 h-2 w-40 bg-gray-300'
+                                                    />
+                                                </>
+                                            ) : (
+                                                <p className='mt-2 text-sm text-zinc-700'>
+                                                    Redirecting, please wait...
+                                                </p>
+                                            )}
+                                        </>
+                                    ) : (
+                                        <>
+                                            <UploadIcon className='h-10 w-10 text-primary' />
+                                            <p className='mt-2 text-sm text-zinc-700'>
+                                                <span className='font-semibold'>
+                                                    Drag and drop
+                                                </span>{' '}
+                                                your image here, or{' '}
+                                                <span className='font-semibold'>
+                                                    click here to browse
+                                                </span>
+                                            </p>
+                                        </>
+                                    )}
 
-                  {isPending ? null : (
-                    <p className="mt-2 text-sm text-zinc-700">
-                      <span className="font-semibold">Supported formats:</span>{' '}
-                      PNG, JPG, JPEG
-                    </p>
-                  )}
-                </div>
-              </div>
+                                    {isPending ? null : (
+                                        <p className='mt-2 text-sm text-zinc-700'>
+                                            <span className='font-semibold'>
+                                                Supported formats:
+                                            </span>{' '}
+                                            PNG, JPG, JPEG
+                                        </p>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </Dropzone>
             </div>
-          )}
-        </Dropzone>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Page;
