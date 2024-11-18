958cfa929700ce4d970b068d0279aef412e7aaf1
Aman Jain <amansjain.aj@gmail.com>
2024-11-18 13:55:59 +0530

    add db interaction functions and prettier format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="bg-slate-50 grainy-light">
+    <div className="bg-slate-50">
       <section>
         <MaxWithWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52 xl:pb-64">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import { Configuration, PrismaClient } from '@prisma/client';
+import { Configuration, Order, PrismaClient, User } from '@prisma/client';
 
 declare global {
     // eslint-disable-next-line no-var
@@ -11,11 +11,12 @@ if (process.env.NODE_ENV !== 'production') {
     global.prisma = client;
 }
 
+// Create a new configuration (without user linkage initially)
 export const createConfiguration = async (
     width: number,
     height: number,
     originalImageUrl: string,
-) => {
+): Promise<Configuration> => {
     return client.configuration.create({
         data: {
             width,
@@ -25,6 +26,7 @@ export const createConfiguration = async (
     });
 };
 
+// Update an existing configuration (e.g., after image cropping)
 export const updateConfiguration = async (
     id: string,
     croppedImageUrl: string,
@@ -36,9 +38,89 @@ export const updateConfiguration = async (
         },
     });
 
-export const getConfigurationById = async (id: string) => {
+// Get a configuration by its ID
+export const getConfigurationById = async (
+    id: string,
+): Promise<Configuration | null> => {
     return client.configuration.findUnique({
         where: { id },
+    });
+};
+
+export const linkConfigurationToUser = async (
+    configurationId: string,
+    userId: string,
+): Promise<Configuration | null> => {
+    return client.configuration.update({
+        where: { id: configurationId },
+        data: { userId },
+    });
+};
+
+export const getConfigurationsByUser = async (
+    userId: string,
+): Promise<Configuration[]> => {
+    return client.configuration.findMany({
+        where: { userId },
+    });
+};
+
+// User
+export const createUser = async (
+    id: string,
+    email: string,
+    name?: string,
+): Promise<User> => {
+    return client.user.create({
+        data: {
+            email,
+            providerId: id,
+            name,
+        },
+    });
+};
+export const getUserByEmail = async (email: string): Promise<User | null> => {
+    return client.user.findUnique({ where: { email } });
+};
+
+export const getUserByProviderId = async (
+    providerId: string,
+): Promise<User | null> => {
+    return client.user.findUnique({
+        where: { providerId },
+    });
+};
+
+export const ensureUser = async (userInfo: {
+    providerId: string;
+    email: string;
+    name?: string;
+}) => {
+    const { email, providerId, name } = userInfo;
+
+    // Check if the user already exists
+    let user = await getUserByEmail(email);
+
+    // If user doesn't exist, create a new one
+    if (!user) {
+        user = await createUser(providerId, email, name);
+    }
+
+    return user;
+};
+
+export const createOrder = async (
+    amount: number,
+    userId: string,
+    configurationId: string,
+): Promise<Order> => {
+    return client.order.create({
+        data: {
+            amount,
+            userId,
+            configurationId,
+            orderStatus: 'awaiting_shipment',
+        },
     });
 };
 
